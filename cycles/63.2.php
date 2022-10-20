4de668383c21770896e5141bc6fb7faed199b4dc
KitaezBakalavr <goshaplekhov@mail.ru>
2022-10-20 05:47:28 +0000

    63.2.php

---
+++ cycles/63.2.php
@@ -0,0 +1,17 @@
+<?php
+	$arr = [
+		'employee1' => 100,
+		'employee2' => 200,
+		'employee3' => 300,
+		'employee4' => 400,
+		'employee5' => 500,
+		'employee6' => 600,
+		'employee7' => 700,
+	];
+    foreach($arr as $key => $elem) {
+        if ($elem <= 400) {
+        $arr[$key] *= 1.1;
+        }
+        echo $arr[$key].'<br>';
+    }
+?>
